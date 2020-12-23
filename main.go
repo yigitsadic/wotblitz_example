7c5b6ed2298385c658a09c1bb8e3e8aacd4c89d3
Yiğit Sadıç <yigitsadic@gmail.com>
2020-12-23 18:24:10 +0300

    added chi

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/yigitsadic/wotblitz_example/graph"
+	"github.com/yigitsadic/wotblitz_example/graph/generated"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3700"
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
